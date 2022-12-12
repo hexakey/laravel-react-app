af36dd7442ee330850a57a54eb6931a5eaa1c8ed
hexakey <ev0.kpx@gmail.com>
2022-12-13 08:59:00 +0900

    [update]サンプルボタン配置

--- src/resources/js/components/Example.jsx
+++ src/resources/js/components/Example.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Button from '@mui/material/Button';
 
 function Example() {
     return (
@@ -7,9 +8,10 @@ function Example() {
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">React導入できたわな？？</div>
+                        <div className="card-header">React導入できたっしょ？</div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">ワイはサンプルコンポーネントちゅうことや</div>
+                        <Button color="success" variant="contained">ワイがMaterial-UIのボタンやな？？</Button> 
                     </div>
                 </div>
             </div>
@@ -18,13 +20,11 @@ function Example() {
 }
 
 export default Example;
+const Index = ReactDOM.createRoot(document.getElementById("app"));
 
-if (document.getElementById('app')) {
-    const Index = ReactDOM.createRoot(document.getElementById("app"));
+Index.render(
+    <React.StrictMode>
+        <Example/>
+    </React.StrictMode>
+)
 
-    Index.render(
-        <React.StrictMode>
-            <Example/>
-        </React.StrictMode>
-    )
-}
