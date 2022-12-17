f589873120d21ff1e7eb53028edf593f04c3200f
hexakey <ev0.kpx@gmail.com>
2022-12-17 21:18:34 +0900

    [update]テーブル表示をMainTableコンポーネントとして切り出し

--- src/resources/js/pages/Home.jsx
+++ src/resources/js/pages/Home.jsx
@@ -1,33 +1,5 @@
-import ReactDOM from 'react-dom';
 import { Button } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-}));
-  
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    '&:last-child td, &:last-child th': {
-        border: 0,
-    },
-}));
+import MainTable from '../components/MainTable'; 
 
 function createData(name, task, editBtn, deleteBtn) {
     return { name, task, editBtn, deleteBtn }; 
@@ -41,7 +13,7 @@ const rows = [
 ];
 
 const header_list = ['名前', 'タスク内容', '編集', '完了'];
-  
+
 function Home() {
     return (
         <div className="container">
@@ -49,33 +21,11 @@ function Home() {
                 <div className="col-md-10">
                     <div className="card">
                         <h1>タスク管理</h1>
-                        <TableContainer component={Paper}>
-                            <Table sx={{ minWidth: 700 }} aria-label="customized table">
-                                <TableHead>
-                                <TableRow>
-                                    {header_list.map((item, index) => (
-                                        <StyledTableCell align="center" key={index}>{item}</StyledTableCell>
-                                    ))}
-                                </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                {rows.map((row, index) => (
-                                    <StyledTableRow key={index}>
-                                        {Object.keys(row).map(function(key, i) {
-                                            return(
-                                                <StyledTableCell align="center" key={i}>{row[key]}</StyledTableCell>
-                                            );
-                                        })}
-                                    </StyledTableRow>
-                                ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
+                        <MainTable header_list={header_list} rows={rows} />  
                     </div>
                 </div>
             </div>
         </div>
-
     );  
 }
 
